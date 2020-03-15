fbf9f0512949510bc45d2fb16f80d3261c8ffc2b
Erik Schierboom <erik_schierboom@hotmail.com>
2020-03-15 17:04:23 +0100

    Add test case for multiple open statements

---
+++ test/Exercism.Representers.FSharp.IntegrationTests/Solutions/Imports/MultipleImports/Fake.fs
@@ -0,0 +1,14 @@
+module Fake
+
+open System
+
+open System.IO
+open System.Collections.Generic
+
+open FsUnit.Xunit
+open Xunit
+
+
+let test () = 
+    Console.Write("Hi ")
+    Console.WriteLine("there!")
